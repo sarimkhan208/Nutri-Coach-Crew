d9749d1f194c36fe5a7d58a0511b1bb9a1dd911e
sarimkhan208 <sarimkhan7275@gmail.com>
2023-05-15 09:31:11 +0530

    done

--- backend/models/User.model.js
+++ backend/models/User.model.js
@@ -3,8 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = mongoose.Schema({
     name:{ type: String, required: true },
     email:{ type: String, required: true, unique: true },
-    password:{ type: String, required: true },
-    role:{ type: String, enum: ["user","admin"], default: "user" }
+    password:{ type: String, required: true }
 },
 {
     versionKey:false
